fcdf3c7135f56fe4d0b0ee79f0088c19612ceea9
Zainabcode-2023 <anasalwajod1996@gmail.com>
2023-05-18 17:32:47 +0300

    trying to finish

--- index.js
+++ index.js
@@ -2,3 +2,31 @@ const employeesJSON = require('./employees.json');
 const managersJSON = require('./managers.json');
 
 // Code Here
+class Employee {
+    constructor(name, position, yearJoined, salary){
+    this.name = name;
+    this.position = position;
+    this.yearJoined = yearJoined;
+    this.salary = salary;
+}
+const idBadge = new idBadge(); 
+{
+// actually i know i wrote it in a wrong way but i think i should nuse the arrow fuction here
+// to assign for a specific value which is first name, which i will write it from what i understood:
+// return this.position = Employee.find(firstName) => firstName.this.name;
+return this.position;
+return this.name;
+    
+}
+
+}
+
+class Manager extends Employee {
+constructor (name, position, yearJoined, salary, bonusPercentage){
+    super(Employee);
+    bonusPercentage = "0.14";
+
+}
+
+
+}
